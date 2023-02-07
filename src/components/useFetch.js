b371b3489136bfa2e7cf5f42e945b2cde02c6817
Naing Lin Aung <nainglinaung@s2t.ai>
2023-02-08 01:34:17 +0630

    react hooks

---
+++ src/components/useFetch.js
@@ -0,0 +1,35 @@
+import {useState,useEffect} from 'react'
+
+function useFetch() {
+  
+    const [loading,setLoading] = useState(false);
+    const [data,setData] =  useState([]);
+    const [error, setError] = useState(null);
+    const [url, setUrl] = useState();
+    useEffect(() => {
+      let myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+      };
+      setLoading(true);
+      fetch(url, requestOptions)
+        .then(response => response.json())
+        .then((res) => {
+            if (res.length > 0) {
+                setData(res);
+            }
+            setLoading(false);
+        })
+        .catch((err) => {
+            setError(err);
+            setLoading(false);
+        });
+    }, [url]);
+
+    return [data,error,loading,setUrl]
+}
+
+export default useFetch
